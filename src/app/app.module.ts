dfb4564080499287da76c441ba669bb65c922906
farzadjamshidi <farzadjamshidicom@yahoo.com>
2023-03-30 20:29:04 +0300

    Initial login api call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './pages/login/login.component';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { UsersV1Tradermade } from './core/repository/tradermade/v1/users.repo';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { LogoutComponent } from './pages/logout/logout.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +23,13 @@ import { LogoutComponent } from './pages/logout/logout.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: 'IUsersRepo', useClass: UsersV1Tradermade }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PostLoginRequest } from '../../core/models/login.model';
+import { IUsersRepo } from '../../core/repository/interfaces/users.interface';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent
+{
+
+  form: FormGroup = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
+
+  constructor(
+    @Inject('IUsersRepo') private usersRepo: IUsersRepo,
+    private router: Router
+  )
+  { }
+
+  submit(): void
+  {
+    const request: PostLoginRequest = this.form.value;
+
+    this.usersRepo.login(request).subscribe({
+      next: (response) =>
+      {
+
+        //save to local storage
+
+        this.router.navigate(['home']);
+      }
+    });
+  }
 
 }
